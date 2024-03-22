5a5b1b73a5f5ebb6392a0ecae40d0576f521151f
Michael Goodnow <mmgoodnow@gmail.com>
2024-03-21 18:26:55 -0600

    fix [object Response] log

--- src/clients/Deluge.ts
+++ src/clients/Deluge.ts
@@ -144,7 +144,7 @@ export default class Deluge implements TorrentClient {
 				cause: networkError,
 			});
 		}
-		console.log(`JSON RESPONSE: ${response}`);
+		console.log("JSON RESPONSE", response);
 		try {
 			json = await response.json();
 		} catch (jsonParseError) {
